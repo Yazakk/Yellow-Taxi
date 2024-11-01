c2210eb611ef42d88525eee4141735f08148514b
Yazakk <yazan.akkad95@gmail.com>
2024-11-01 15:20:45 +0400

    update issues

--- Solutions/Part_1.sql
+++ Solutions/Part_1.sql
@@ -1,10 +1,10 @@
 -- 1.Total Revenue Calculation
 SELECT SUM(fare_amount + extra + improvement_surcharge + congestion_surcharge + airport_fee) AS tpep_revenue
-FROM "NYC_Yellow_Taxi_Trip_20204_01"
+FROM "NYC_Yellow_Taxi_Trip_20203_01"
 WHERE payment_type IN (1, 2, 5)
   AND tpep_dropoff_datetime BETWEEN 
-      (SELECT MAX(tpep_dropoff_datetime) - INTERVAL '30 days' FROM "NYC_Yellow_Taxi_Trip_20204_01")
-      AND (SELECT MAX(tpep_dropoff_datetime) FROM "NYC_Yellow_Taxi_Trip_20204_01");
+      (SELECT MAX(tpep_dropoff_datetime) - INTERVAL '30 days' FROM "NYC_Yellow_Taxi_Trip_20203_01")
+      AND (SELECT MAX(tpep_dropoff_datetime) FROM "NYC_Yellow_Taxi_Trip_20203_01");
 
 -- Result 65357329.3400042
 
@@ -12,11 +12,11 @@ WHERE payment_type IN (1, 2, 5)
 
 SELECT "PULocationID", 
        SUM(fare_amount + extra + improvement_surcharge + congestion_surcharge + airport_fee) AS tpep_revenue
-FROM "public"."NYC_Yellow_Taxi_Trip_20204_01"
+FROM "public"."NYC_Yellow_Taxi_Trip_20203_01"
 WHERE payment_type IN (1, 2, 5)
   AND tpep_dropoff_datetime BETWEEN 
-      (SELECT MAX(tpep_dropoff_datetime) - INTERVAL '30 days' FROM "public"."NYC_Yellow_Taxi_Trip_20204_01")
-      AND (SELECT MAX(tpep_dropoff_datetime) FROM "public"."NYC_Yellow_Taxi_Trip_20204_01")
+      (SELECT MAX(tpep_dropoff_datetime) - INTERVAL '30 days' FROM "public"."NYC_Yellow_Taxi_Trip_20203_01")
+      AND (SELECT MAX(tpep_dropoff_datetime) FROM "public"."NYC_Yellow_Taxi_Trip_20203_01")
 GROUP BY "PULocationID"
 ORDER BY tpep_revenue DESC
 LIMIT 3;
